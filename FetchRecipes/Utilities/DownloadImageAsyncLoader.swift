5c777adca0e9a26eaaa1ec2e6bba127763026547
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-08-07 12:56:31 -0400

    [Bug] In DownloadImageAsyncLoader, the url property was erroneously force unwrapped. Redefined url property to a URL optional.

--- FetchRecipes/Utilities/DownloadImageAsyncLoader.swift
+++ FetchRecipes/Utilities/DownloadImageAsyncLoader.swift
@@ -10,10 +10,10 @@ import SwiftUI
 
 class DownloadImageAsyncLoader {
     
-    let url: URL
+    let url: URL?
     
     init(urlString: String) {
-        url = URL(string: urlString)!
+        self.url = URL(string: urlString)
     }
     
     func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
@@ -31,9 +31,15 @@ class DownloadImageAsyncLoader {
     func downloadWithAsync() async throws -> UIImage?{
         
         do {
-            //await keyword is a suspension point on a task, it says suspend the task until a response is sent back.
+            //await keyword is a suspension point on a task,
+            //suspend the task until a response is sent back.
             //url session is executed immediately but the response is handled
             //asyncronously
+            //self.url is an optional, let's verify it is valid
+            guard let url = self.url
+            else {
+                return nil
+            }
             let (data, response) = try await URLSession.shared.data(from: url, delegate: nil)
             let image = handleResponse(data: data, response: response)
 //            print("image downloaded")
@@ -41,7 +47,6 @@ class DownloadImageAsyncLoader {
         } catch {
             throw error
         }
-        
     }
 }
 
